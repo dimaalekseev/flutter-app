4e7b9969b4b1ca7a412e002cc2b042ae9f168d75
Дима Алексеев <dimaalekseev@Admin.Dlink>
2021-02-26 05:43:18 +0200

    done

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(SimpleWidget());
+
+class SimpleWidget extends StatefulWidget {
+  @override
+  _SimpleWidgetState createState() => _SimpleWidgetState();
+}
+
+class _SimpleWidgetState extends State<SimpleWidget> {
+  //private variable for generating random color
+  Color _color;
+
+  //variable for calculating how many times user tapped the screen
+  int counter = 0;
+
+  //method for generating our random color using state
+  void randomColor() {
+    setState(() {
+      _color = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+          .withOpacity(1.0);
+    });
+  }
+
+  @override
+  void initState() {
+    randomColor();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      //it's better to use an arrow function, not a usual method, in order to execute only when it called, not every time program runs
+      onTap: () => {randomColor(), ++counter},
+      child: Container(
+        alignment: Alignment.center,
+        color: _color,
+        child: Text(
+          "Hey there $counter times!",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 30, color: Colors.purple),
+        ),
+      ),
+    );
+  }
+}
